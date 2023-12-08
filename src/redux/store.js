0ec378376cbb8ba0e1bb77bf626ed919191650c1
Ullas Chandrakar <ullaschandrakar2404@gmail.com>
2023-12-08 23:26:40 +0530

    redux

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
 import productSlice from './slices/productSlice';
+import cartSlice from './slices/cartSlice';
 
 const store = configureStore({
     reducer:{
         product: productSlice,
+        cart: cartSlice
     }
 });
 
